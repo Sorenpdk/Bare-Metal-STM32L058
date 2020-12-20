83533fa634b01a66245134b9ac61dc4d5d0b33f0
Søren <Sorenporksen@hotmail.com>
2020-12-20 11:40:51 +0100

    Added first instance of ADC project

---
+++ ADC/Inc/customclock.h
@@ -0,0 +1,17 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef CUSTOMCLOCK_H
+#define CUSTOMCLOCK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx.h"
+
+
+
+extern void CustomClock_init(void);
+
+
+#endif // CUSTOMCLOCK_H
