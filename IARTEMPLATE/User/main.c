a476bcdea266cb2310e55ddb285300ad088897da
Søren <Sorenporksen@hotmail.com>
2020-12-20 11:40:05 +0100

    Added template for IAREWARM projects

---
+++ IARTEMPLATE/User/main.c
@@ -0,0 +1,40 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  -
+  * @brief   This file implements the following:
+  *          - Baremetal implementation using the CMSIS library for Cortex-M0+ 
+  *             
+  *            
+  *          
+  ******************************************************************************
+ */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx.h"
+/* Private define ------------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+/**
+  * Brief   Main program.
+  * Param   None
+  * Retval  None
+  */
+int main(void)
+{
+ 
+  while (1)
+  {
+   
+  }
+}
+
+
+
+
+
+
+/* https://www.st.com/resource/en/reference_manual/dm00095744-ultralowpower-stm32l0x3-advanced-armbased-32bit-mcus-stmicroelectronics.pdf */
+
