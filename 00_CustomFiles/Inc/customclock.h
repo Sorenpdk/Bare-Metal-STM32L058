fb42453af0a93e256e80a74c398ea2136447b08c
Søren <Sorenporksen@hotmail.com>
2021-01-06 11:42:06 +0100

    Added customfiles folder for custom header files

---
+++ 00_CustomFiles/Inc/customclock.h
@@ -0,0 +1,23 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef CUSTOMCLOCK_H
+#define CUSTOMCLOCK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx.h"
+#include "customdelay.h"
+#include "system_stm32l0xx.h" // For SystemCoreClockUpdate
+
+/* Private define ------------------------------------------------------------*/
+#define HSI_TIMEOUT_VALUE              ((uint32_t)100)  /* 100 ms */
+#define REGULATOR_TIMEOUT_VALUE        ((uint32_t)500)  /* 500 ms */
+#define CLOCKSWITCH_TIMEOUT_VALUE      ((uint32_t)5000) /* 5 s    */
+
+
+extern void customClock_init(void);
+
+
+#endif // CUSTOMCLOCK_H
